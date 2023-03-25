55c3d3fabcbb89561f0339b80ce1bf7d6b88f5f9
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-03-26 00:51:10 +0900

    Make player move forward in the direciton of a camera

--- Assets/Scripts/RelativeMovement.cs
+++ Assets/Scripts/RelativeMovement.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class RelativeMovement : MonoBehaviour
 {
     [SerializeField] Transform target;
 
     public float rotSpeed = 15.0f;
+    public float moveSpeed = 6.0f;
+
+    private CharacterController charController;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        charController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -28,9 +32,14 @@ public class RelativeMovement : MonoBehaviour
             var forward = Vector3.Cross(right, Vector3.up);
 
             movement = (right * horInput) + (forward * verInput);
+            movement *= moveSpeed;
+            movement = Vector3.ClampMagnitude(movement, moveSpeed);
 
             var direction = Quaternion.LookRotation(movement);
             transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotSpeed * Time.deltaTime);
         }
+
+        movement *= Time.deltaTime;
+        charController.Move(movement);
     }
 }
