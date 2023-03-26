6b5f44a29ae071375acf666f199d597383d34050
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-03-26 20:49:23 +0900

    Add jump logic to RelativeMovement component

--- Assets/Scripts/RelativeMovement.cs
+++ Assets/Scripts/RelativeMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 [RequireComponent(typeof(CharacterController))]
 public class RelativeMovement : MonoBehaviour
@@ -10,11 +11,19 @@ public class RelativeMovement : MonoBehaviour
     public float rotSpeed = 15.0f;
     public float moveSpeed = 6.0f;
 
+    public float jumpSpeed = 15.0f;
+    public float gravity = -9.8f;
+    public float terminalVelocity = -10.0f;
+    public float minFall = -1.5f;
+
+    private ControllerColliderHit contact;
     private CharacterController charController;
+    private float verSpeed;
 
     // Start is called before the first frame update
     void Start()
     {
+        verSpeed = minFall;
         charController = GetComponent<CharacterController>();
     }
 
@@ -39,7 +48,43 @@ public class RelativeMovement : MonoBehaviour
             transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotSpeed * Time.deltaTime);
         }
 
+        var hitGround = false;
+        if (verSpeed < 0 && Physics.Raycast(transform.position, Vector3.down, out var hit))
+        {
+            var check = (charController.height + charController.radius) / 1.9f;
+            hitGround = hit.distance <= check;
+        }
+
+        if (hitGround)
+        {
+            if (Input.GetButtonDown("Jump"))
+                verSpeed = jumpSpeed;
+            else
+                verSpeed = minFall;
+        }
+        else
+        {
+            verSpeed += gravity * 5 * Time.deltaTime;
+            if (verSpeed < terminalVelocity)
+                verSpeed = terminalVelocity;
+
+            // Raycast didn't detect the ground, but the capsule is touching the ground
+            if (charController.isGrounded)
+            {
+                if (Vector3.Dot(movement, contact.normal) < 0)
+                    movement = contact.normal * moveSpeed;
+                else
+                    movement += contact.normal * moveSpeed;
+            }
+        }
+
+        movement.y = verSpeed;
         movement *= Time.deltaTime;
         charController.Move(movement);
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        contact = hit;
     }
 }
